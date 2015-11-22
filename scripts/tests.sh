98068a2d1fbdd39711257bc1d54d1bd0c340323d
SykoraErik <sykora.erik@gmail.com>
2015-11-23 00:16:00 +0100

    Add Travis-ci configuration

---
+++ scripts/tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# test script for .travis.yml
+ 
+set -e
+COMMIT_TAG=$(git tag --contains "${TRAVIS_COMMIT}")
+if [[ $COMMIT_TAG =~ release.* ]]; then 
+git clone https://github.com/SykoraErik/BachelorThesis.git testRepository
+cd testRepository
+mvn -Pphantomjs -Dbase.url=http://staging-new.slepemapy.cz/ clean verify;
+fi
