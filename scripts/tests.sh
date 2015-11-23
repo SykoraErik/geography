01dec0b8cbc33739bbff3b5ff36ec9ce5e9e26df
SykoraErik <sykora.erik@gmail.com>
2015-11-23 07:47:43 +0100

    Test if script is working

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 # test script for .travis.yml
 
-set -e
-
-COMMIT_TAG=$(git tag --contains "${TRAVIS_COMMIT}")
-
-if [[ $COMMIT_TAG =~ release.* ]]; then
 git clone https://github.com/SykoraErik/BachelorThesis.git testRepository
 cd testRepository
-mvn -Pphantomjs -Dbase.url=http://staging-new.slepemapy.cz/ clean verify;
-fi
+mvn -Pphantomjs -Dbase.url=http://staging-new.slepemapy.cz/ clean verify
